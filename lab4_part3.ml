b459da5eb8b6bf8cef39f2d712d38ede31c4af89
Rebecca Lisk <rebeccalisk@Rebeccas-MacBook-Pro.local>
2018-02-22 12:43:56 -0500

    lab part 3

--- lab4_part3.ml
+++ lab4_part3.ml
@@ -60,16 +60,16 @@ module IntListStack =
     type stack = int list
 
     (* Returns an empty stack *)
-    let empty () : stack = failwith "not implemented"
+    let empty () : stack = []
 
     (* Add an element to the top of the stack *)
-    let push (i : int) (s : stack) : stack = failwith "not implemented"
+    let push (i : int) (s : stack) : stack = s @ [i]
 
     (* Return the value of the topmost element on the stack *)
-    let top (s : stack) : int = failwith "not implemented"
+    let top (s : stack) : int = List.hd(List.rev s)
 
     (* Return a modified stack with the topmost element removed *)
-    let pop (s : stack) : stack = failwith "not implemented"
+    let pop (s : stack) : stack = List.tl(List.rev s)
   end ;;
 
 (* Now let's use this implementation and consider some implications.
@@ -81,14 +81,14 @@ the values "5" and then "1" pushed in that order.
 ......................................................................*)
 
 let small_stack () : IntListStack.stack =
-  failwith "not implemented" ;;
+  IntListStack.push 1 (IntListStack.push 5 []) ;;
 
 (*......................................................................
 Exercise 3C: Now, use IntListStack methods to write an expression that
 defines last_el as the value of the topmost element from small_stack.
 ......................................................................*)
 
-let last_el = 0;;
+let last_el = IntListStack.top(small_stack());;
 
 (* Based on our requirements above, what should last_el contain?
 
@@ -108,7 +108,7 @@ methods.
 ......................................................................*)
 
 let invert_stack (s : IntListStack.stack) : IntListStack.stack =
-  failwith "not implemented" ;;
+  List.rev s;;
 
 (* Now what would be the result of the top operation on invert_stack?
 
@@ -118,7 +118,7 @@ top value from a small_stack inverted with invert_stack and store it
 in bad_el.
 ......................................................................*)
 
-let bad_el = 0 ;;
+let bad_el = IntListStack.top(invert_stack(small_stack())) ;;
 
 (* This is bad. We have broken through the *abstraction barrier*
 defined by the IntListStack module. You may wonder: "if I know that
@@ -156,6 +156,11 @@ that's the type you used in your implementation.
 
 module type INT_STACK =
   sig
+    type stack
+    val empty : unit -> stack
+    val push : int -> stack -> stack
+    val pop : stack -> stack
+    val top : stack -> int
   end ;;
 
 (* Now, we'll apply the INT_STACK interface to the IntListStack. *)
@@ -172,4 +177,4 @@ perform list operations directly on it, which means the stack
 preserves its abstraction barrier.
 ......................................................................*)
 
-let safe_stack () = failwith "not implemented" ;;
+let safe_stack () = SafeIntListStack.push 1 (SafeIntListStack.push 5 (SafeIntListStack.empty())) ;;
